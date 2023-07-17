bb29fec7903192332c1d09091b5b9bd8c95e30aa
Khalil Al-Battashi <75760784+KhalilAlbattashi@users.noreply.github.com>
2023-07-17 15:25:16 +0400

    added changes to the view

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@
     <!-- Form to add new todo -->
     <form @submit.prevent="addTodo" class="flex mb-4">
       <input type="text" v-model="newTodo" placeholder="Enter a new todo" class="px-4 py-2 mr-2 border border-gray-300 rounded-md" />
-      <select v-model="newTodoGroup" class="px-4 py-2 border border-gray-300 rounded-md">
+      <select v-model="newTodoGroup" class="px-4 me-2 py-2 border border-gray-300 rounded-md">
         <option value="">No Group</option>
         <option >Work</option>
         <option >Chores</option>
@@ -19,13 +19,13 @@
     <ul>
       <li v-for="todo in todos" :key="todo.id" class="flex items-center mb-2">
         <span class="mr-2">{{ todo.text }}</span>
-        <span v-if="todo.group" :class="getGroupStyle(todo.group)" class="text-sm text-white rounded-full px-2 py-1">{{ todo.group }}</span>
-        <button @click="deleteTodo(todo.id)" class="px-2 py-1 bg-red-500 text-white rounded-md">Delete</button>
-        <button @click="editTodo(todo)" class="px-2 py-1 bg-blue-500 text-white rounded-md">Edit</button>
+        <span v-if="todo.group" :class="getGroupStyle(todo.group)" class="me-2 text-sm text-white rounded-full px-2 py-1">{{ todo.group }}</span>
+        <button @click="deleteTodo(todo.id)" class="me-2 px-2 py-1 bg-red-500 text-white rounded-md">Delete</button>
+        <button @click="editTodo(todo)" class="me-2 px-2 py-1 bg-blue-500 text-white rounded-md">Edit</button>
       </li>
     </ul>
 
-    <!-- Edit todo modal -->
+    <!-- Edit todo Form will bw shown when the edit button is clicked -->
     <div v-if="showEditModal" class="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-50">
       <div class="bg-white p-4">
         <h2 class="text-lg font-bold mb-2">Edit Todo</h2>
@@ -37,7 +37,7 @@
           <option >Personal</option>
           <option >Home</option>
         </select>
-        <div class="flex justify-end">
+        <div class="flex justify-end">  
           <button @click="updateTodo" class="px-4 py-2 bg-blue-500 text-white rounded-md mr-2">Update</button>
           <button @click="cancelEdit" class="px-4 py-2 bg-gray-300 text-gray-800 rounded-md">Cancel</button>
         </div>
@@ -50,13 +50,7 @@
 export default {
   data() {
     return {
-      todos: [
-        { id: 1, text: 'Buy groceries', group: 'Home' },
-        { id: 2, text: 'Walk the dog', group: 'Home' },
-        { id: 3, text: 'Do laundry', group: 'Chores' },
-        { id: 4, text: 'Do Nuxt ', group: 'Work' },
-        { id: 5, text: 'Use Tailwind ', group: 'Personal' }
-      ],
+      todos: [],
       newTodo: '',
       newTodoGroup: '',
       showEditModal: false,
@@ -64,17 +58,6 @@ export default {
       editTodoText: '',
       editTodoGroup: '',
     };
-  },
-  computed: {
-    groups() {
-      const groupsSet = new Set();
-      this.todos.forEach((todo) => {
-        if (todo.group) {
-          groupsSet.add(todo.group);
-        }
-      });
-      return Array.from(groupsSet).map((group) => ({ value: group, label: group }));
-    },
   },
   methods: {
     addTodo() {
@@ -84,6 +67,7 @@ export default {
         group: this.newTodoGroup,
       };
       this.todos.push(newTodo);
+      // reset the values inside the form
       this.newTodo = '';
       this.newTodoGroup = '';
     },
@@ -93,7 +77,7 @@ export default {
     editTodo(todo) {
       this.editTodoId = todo.id;
       this.editTodoText = todo.text;
-      this.editTodoGroup = todo.group || '';
+      this.editTodoGroup = todo.group ;
       this.showEditModal = true;
     },
     updateTodo() {
@@ -117,7 +101,7 @@ export default {
         Work: 'bg-yellow-500',
         Personal: 'bg-purple-500',
       };
-      return styles[group] || 'bg-gray-500';
+      return styles[group];
     },
   },
 };
